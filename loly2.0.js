42d6b5ec6031b301ed7000988e980b489502c979
walter239911 <garciawald@gmail.com>
2022-05-04 23:09:16 -0300

    arreclamos un pequeño horror

--- loly2.0.js
+++ loly2.0.js
@@ -36,7 +36,7 @@ contenedor.forEach((el)=>{
             if(el.textContent.includes("precio")){
                 texthtml = `
                 <h1>peluqueria</h1>
-                <div class="contenedor-Estetica">
+                <div class="contenedor-Precios">
                 <ul>
                 <li>corte $300</li>
                 <li>tinte $1500</li>
@@ -63,7 +63,7 @@ contenedor.forEach((el)=>{
             }
             if(el.textContent.includes("estetica")){
                ` <h1>estetica</h1>
-               <div class="contenedor-Precios">
+               <div class="contenedor-Estetica">
                <ul class="Contenedor-Estetica-1">
                <li>por zona $300</li>
                <li>por hora $1500</li>
